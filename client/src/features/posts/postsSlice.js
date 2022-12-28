86cdead6249d6985d3f77c60aa858d6dbaff858d
vishal-Tx <vishalguptaa2z111@gmail.com>
2022-12-28 18:35:11 +0530

    createEntityAdapter applied in post slice.

--- client/src/features/posts/postsSlice.js
+++ client/src/features/posts/postsSlice.js
@@ -1,18 +1,23 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from "@reduxjs/toolkit";
 import axios from "axios";
 import { fetchPosts, fetchPostsBySearch, fetchPost } from "../api";
 
 const url = "http://localhost:3001/posts";
 
-const initialState = {
-  posts: [],
+const postsAdapter = createEntityAdapter({});
+
+const initialState = postsAdapter.getInitialState({
   post: null,
   isLoading: true,
   detailsLoading: true,
   error: null,
   currentPage: 1,
   numberOfPages: 1,
-};
+});
 // console.log("initialState.posts", initialState.posts);
 
 export const getPosts = createAsyncThunk(
@@ -72,40 +77,45 @@ const postsSlice = createSlice({
   initialState,
   reducers: {
     clearAll: (state, action) => {
-      state.posts = [];
+      // state.posts = [];
     },
     create: (state, action) => {
       // console.log("action", action.payload);
-      state.posts = [...state.posts, action.payload];
+      postsAdapter.addOne(state, action.payload);
+      // state.posts = [...state.posts, action.payload];
     },
     update: (state, action) => {
       state.detailsLoading = true;
       // console.log("actionLike", action.payload);
       // console.log("state.post", state.post);
 
-      state.posts = state.posts.map((post) =>
-        post._id === action.payload._id ? action.payload : post
-      );
+      // state.posts = state.posts.map((post) =>
+      //   post._id === action.payload._id ? action.payload : post
+      // );
+      postsAdapter.upsertOne(state, action.payload);
       state.post = action.payload;
       state.detailsLoading = false;
     },
     remove: (state, action) => {
-      state.posts = state.posts.filter(
-        (post) => post._id !== action.payload._id
-      );
+      // state.posts = state.posts.filter(
+      //   (post) => post._id !== action.payload._id
+      // );
+      postsAdapter.removeOne(state, action.payload._id);
     },
     like: (state, action) => {
-      state.posts = state.posts.map((post) =>
-        post._id === action.payload._id ? action.payload : post
-      );
+      // state.posts = state.posts.map((post) =>
+      //   post._id === action.payload._id ? action.payload : post
+      // );
+      postsAdapter.upsertOne(state, action.payload);
     },
     addComment: (state, action) => {
       // console.log("action.payload", action.payload);
       // console.log("state.post", state.post);
 
-      state.posts = state.posts.map((post) =>
-        post._id === action.payload._id ? action.payload : post
-      );
+      // state.posts = state.posts.map((post) =>
+      //   post._id === action.payload._id ? action.payload : post
+      // );
+      postsAdapter.upsertOne(state, action.payload);
       state.post = action.payload;
     },
   },
@@ -116,7 +126,8 @@ const postsSlice = createSlice({
     [getPosts.fulfilled]: (state, action) => {
       state.isLoading = false;
 
-      state.posts = action.payload.data;
+      // state.posts = action.payload.data;
+      postsAdapter.upsertMany(state, action.payload.data);
       state.currentPage = action.payload.currentPage;
       state.numberOfPages = action.payload.numberOfPages;
       // console.log("allposts", action.payload);
@@ -154,7 +165,9 @@ const postsSlice = createSlice({
     [getPostsBySearch.fulfilled]: (state, action) => {
       state.isLoading = false;
       // console.log("actionS", action.payload);
-      state.posts = action.payload;
+      postsAdapter.upsertMany(state, action.payload);
+      // state.posts = action.payload;
+
       state.error = null;
     },
     [getPostsBySearch.rejected]: (state, action) => {
